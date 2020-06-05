f1c47bedf8490e1b4a2b84a0c4f7c6762c047bee
unknown <souravrao3110@gmail.com>
2020-06-05 22:58:22 +0530

    added sublime-cheats

---
+++ sublime.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);	
+  freopen("output.txt","w",stdout);	
+#endif
+   
+ //write code here   
+
+   
+int a=10;
+int ans = a +11;
+
+cout<<ans;
+}
