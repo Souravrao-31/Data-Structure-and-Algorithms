f1b0ff4a19583a84b81e9ed1ab314f98ca18af73
Sourav Rao <souravrao3110@gmail.com>
2020-08-22 23:03:43 +0530

    sorting

--- Sorting/Count_Sort.cpp
+++ Sorting/Count_Sort.cpp
@@ -2,12 +2,13 @@
 
 using namespace std;
 
-void sort(int *arr,int n){
+#define ll long long
+void sort(int *arr,ll n){
 
     //largest in the array assuming no > 0
     int largest = -1;
 
-    for(int i=0;i<n;i++){
+    for(ll i=0;i<n;i++){
         largest = max(largest,arr[i]);
     }
 
@@ -15,14 +16,14 @@ void sort(int *arr,int n){
     //create freq
     int *freq = new int[largest+1] {0};
 
-    for(int i=0;i<n;i++){
+    for(ll i=0;i<n;i++){
        freq[arr[i]]++;
     }
 
     //put the elements back into the array a by reading array
-    int j=0;
+    ll j=0;
 
-    for(int i=0;i<largest;i++){
+    for(ll i=0;i<largest;i++){
         while(freq[i]>0){
              arr[j] = i;
              freq[i]--;
@@ -35,19 +36,19 @@ void sort(int *arr,int n){
 }
 
 int main(){
-    //int arr[] ={8,5,2,1,3,4,5,25,88,2,1,45,6,3,8,45,88};
+   
 
-   int n;
+   ll n;
    cin>>n;
 
    int arr[n];
-    for(int i=0;i<n;i++){
+    for(ll i=0;i<n;i++){
         cin>>arr[i];
     }
 
     sort(arr,n);
 
-    for(int i=0;i<n;i++){
+    for(ll i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
 
---
+++ Sorting/Sort_game.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool myComp(pair<string, int> p1, pair<string, int> p2)
+{
+    if(p1.second == p2.second)
+    {
+        return p1.first < p2.first;
+    }
+    return p1.second > p2.second;
+}
+int main()
+{
+    int min_salary,n;
+    pair<string, int> emp[100005];
+    cin>>min_salary;
+    cin>>n;
+    string name;
+    int salary;
+    for(int i=0;i<n;i++)
+    {
+        cin>>name>>salary;
+        emp[i].first = name;
+        emp[i].second = salary;
+    }
+    sort(emp, emp+n, myComp);
+    for(int i=0;i<n;i++)
+    {
+        if(emp[i].second >= min_salary)
+            cout<<emp[i].first<<" "<<emp[i].second<<endl;
+    }
+    return 0;
+}
