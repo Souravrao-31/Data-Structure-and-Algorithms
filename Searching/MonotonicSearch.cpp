8e3158bbb9df6e0c20df79d38a66761a28309a60
Sourav Rao <souravrao3110@gmail.com>
2020-11-08 11:27:51 +0530

    updated square root BN with bitasking sol

--- Searching/MonotonicSearch.cpp
+++ Searching/MonotonicSearch.cpp
@@ -50,6 +50,36 @@ float Square_root(int n,int p){
    return ans;
 
 }
+
+class Solution {
+public:
+    int mySqrt(int x) {
+         
+        	if (x < 2) return x;
+		
+		uint64_t mult = 0;
+		int count = 0;
+		
+		//have mult start with half the bits of the other, if that's 0 or odd, then +1 bits
+		while ((x >> (31 - count) & 1u) == 0) count++;
+		count = (32 - count)/2 - ((count+1) % 2);
+		
+		//set all mult bits to 1 from count to beginning
+		mult |= (~(0u) >> (31 - count));
+
+		//while mult is greater, set smaller bits to 0
+		while (mult * mult > x && count > 0) {
+			count--;
+			mult ^= (1u << count);
+			
+			//mult^2 is less than x but has too large of a difference, reset bit back to 1
+			if (mult * mult + (mult * 2) < x) mult |= (1u << count);
+		}
+		return mult;
+        
+        
+    }
+};
 int main(){
     
     int n;
