4f0d79af3a1e9138731954e83694422ace4e6de1
Sourav <souravrao3110@gmail.com>
2021-01-28 10:18:45 +0530

    Changes

--- Assignment.py
+++ Assignment.py
@@ -1,34 +1,32 @@
-# Write Python3 code here 
-# collection module used counting in dic for value and keys 
-from collections import Counter 
+import sys
+import math
+import bisect
+from sys import stdin,stdout
+from math import gcd,floor,sqrt,log
+from collections import defaultdict as dd
+from bisect import bisect_left as bl,bisect_right as br
+from collections import Counter
 
-#t = int(input())
+test_cases = int(input())
 
-tweet_names = ["sachin tweet_id_1", 
-			"sehwag tweet_id_2", 
-			"sachin tweet_id_3", 
-			] 
-	
-uniq_names = [pref_names.split()[0] for
-			pref_names in tweet_names] 
+user_name = []
 
-times = Counter(uniq_names) 
-repeat = times.values() 
 
-for element in set(repeat): 
-	dupl = ([(key, value) for
-			key, value in sorted(times.items()) if
-			value == element]) 
-	
-	if len(dupl) > 1: 
-		for (key, value) in dupl: 
-			print (key,'',value) 
-	max_value = max(times.values()) 
-	temp_max_result = [(key, value) for
-					key, value in sorted(times.items()) if
-					value == max_value] 
-	
-	if temp_max_result != dupl: 
-		for (key,value) in temp_max_result: 
-			print (key,'',value) 
-			
+class user:
+ def __init__(self,Player_names): 
+    self.Player_names = Player_names
+    counter = Counter(self.Player_names)   
+    self.max_user = counter.most_common()
+    print(self.max_user)
+  
+    
+for x in range(0,test_cases):
+    tweets_cnt = int(input())
+  
+    for y in range(0,tweets_cnt) :
+        tweets = str(input())
+        dum = list(tweets.split(" "))
+      
+        user_name.append(dum[0])
+
+us = user(user_name)
