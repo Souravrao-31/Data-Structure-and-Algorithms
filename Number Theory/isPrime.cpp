e8387aa3b7a80903b77c4fe128da97552c301a9e
Sourav <souravrao3110@gmail.com>
2021-03-05 12:16:09 +0530

    more accurate way

--- Number Theory/isPrime.cpp
+++ Number Theory/isPrime.cpp
@@ -1,13 +1,23 @@
+#include<iostream>
+using namespace std;
+
 bool isPrime(int n){
-    if(n<=1)
+    if(n==1)
         return false;
-    if(n==2)
+    if(n==2|| n==3)
         return true;
-    if(n%2==0)
+    if(n%2==0 || n%3==0)
         return false;
-    for(int i=3;i*i<=n;i+=2){
-        if(n%i==0)
+    for(int i=5;i*i<=n;i+=6){
+        if(n%i==0 || n% (i+2) == 2)
             return false;
     }
     return true;
+}
+
+int main(){
+    int n; cin>>n;
+    if(isPrime(n)) cout<<"Yes";
+    else cout<<"No";
+    return 0;
 }
