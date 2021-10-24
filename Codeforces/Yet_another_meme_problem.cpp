5543d634a194700c04458e9d8a117633d4d5d29b
sherlocked01 <81800229+sherlocked01@users.noreply.github.com>
2021-10-24 14:54:04 +0530

    Update Yet_another_meme_problem.cpp

--- Codeforces/Yet_another_meme_problem.cpp
+++ Codeforces/Yet_another_meme_problem.cpp
@@ -1,3 +1,9 @@
+/*Problem Statement - You are given two integers A and B, calculate the number of pairs (a,b) such that 1≤a≤A, 1≤b≤B, 
+and the equation a⋅b+a+b=conc(a,b) is true; conc(a,b) is the concatenation of a and b (for example, conc(12,23)=1223, 
+conc(100,11)=10011). a and b should not contain leading zeroes.*/
+
+//Problem Link - https://codeforces.com/contest/1288/problem/B
+
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
